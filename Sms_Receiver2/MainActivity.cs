dc917ca02a8524ce3b8fccf8db2816819e67707a
owais19 <owais19x@gmail.com>
2021-02-02 16:16:48 +0500

    Add project files.

---
+++ Sms_Receiver2/MainActivity.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Telephony;
+using Android.Provider;
+using Android.Util;
+using Java.Lang;
+using System.Text.RegularExpressions;
+using Xamarin.Essentials;
+using System;
+
+
+namespace Sms_Receiver2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        public Receiver1 _receiver = new Receiver1();              // Receiver class 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+
+            translateButton.Click += (s, e) =>
+            {
+
+                translatedPhoneWord.Text = _receiver.message;
+                // Toast.MakeText(ApplicationContext, _receiver.address + ", " + _receiver.message, ToastLength.Short).Show();     //showing a Toast again 
+
+            };
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            RegisterReceiver(_receiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+
+        }
+
+    }
+}
---
+++ Sms_Receiver2/Receiver.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Telephony;
+using Android.Provider;
+
+namespace Sms_Receiver2
+{
+    [BroadcastReceiver(Label = "SMS Receiver")]
+    [IntentFilter(new[] { "android.provider.Telephony.SMS_RECEIVED" }, Priority = (int)IntentFilterPriority.HighPriority)]
+    public class Receiver1 : BroadcastReceiver
+    {
+        public string message, address = "";
+        public static readonly string INTENT_ACTION = "android.provider.Telephony.SMS_RECEIVED";
+        public override void OnReceive(Context context, Intent intent)
+        {
+
+            if (intent.HasExtra("pdus"))
+            {
+                var smsArray = (Java.Lang.Object[])intent.Extras.Get("pdus");
+                foreach (var item in smsArray)
+                {
+                    var sms = SmsMessage.CreateFromPdu((byte[])item);
+                    address = sms.OriginatingAddress;
+                    message = sms.MessageBody;
+                    Toast.MakeText(context, "Number :" + address + "Message : " + message, ToastLength.Short).Show();
+
+                }
+            }
+
+           
+        }
+    }
+}
